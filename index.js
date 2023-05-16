4bd97f27313bff345c1741cca12c858bdbc174ef
Jotechno <jstamayotoro@hotmail.com>
2023-05-16 14:53:08 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Configurar el motor de vistas
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'public', 'views'));
+
+// Ruta para renderizar la página disponibilidad.hbs
+app.get('/disponibilidad', (req, res) => {
+  res.render('disponibilidad');
+});
+
+// Ruta para archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Iniciar el servidor
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor en ejecución en http://localhost:${port}`);
+});
---
+++ public/js/validar.js
@@ -0,0 +1,41 @@
+
+
+const validateForm = () => {
+    const departingDateInput = document.getElementById('fechaSalida');
+    const returningDateInput = document.getElementById('fechaLlegada');
+
+    if (departingDateInput.value.trim() === '' || returningDateInput.value.trim() === '') {
+        alert('Por favor, selecciona fechas de salida y llegada.');
+        return;
+    }
+
+    const validateDatesResult = validateDates();
+
+    if (validateDatesResult) {
+        // Ejecutar la lógica adicional para mostrar los vuelos o el mensaje de error
+        alert('Mostrar vuelos');
+    }
+};
+
+const validateDates = () => {
+    const departingDate = new Date(document.getElementById('fechaSalida').value);
+    const returningDate = new Date(document.getElementById('fechaLlegada').value);
+    const currentDate = new Date();
+    let errorMessage = '';
+
+    if (departingDate <= currentDate) {
+        errorMessage = 'La fecha de salida debe ser igual o posterior a la fecha actual.';
+    } else if (departingDate > returningDate) {
+        errorMessage = 'La fecha de salida no puede ser posterior a la fecha de regreso';
+    }
+
+    if (errorMessage) {
+        alert(errorMessage);
+        return false;
+    } else {
+        return true;
+    }
+};
+
+document.getElementById('show-flights-btn').addEventListener('click', validateForm);
+
